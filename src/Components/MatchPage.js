2169e1369a3d8c09f00e59ffd6406cb815f45ed5
yichl42 <86950411+yichl42@users.noreply.github.com>
2023-11-22 01:32:41 -0800

    updates interactivity

--- src/Components/MatchPage.js
+++ src/Components/MatchPage.js
@@ -1,22 +1,44 @@
-
+import React, { useState } from 'react';
 
 function MatchPage({ cars }) {
-    return (
-      <div>
-        <h1>Car Filter and Match</h1>
-        {/* Filter form can be static for now */}
-        <section>
-          {/* Form content */}
-        </section>
-        <section>
-          <h2>Matching Cars</h2>
-          <ul>
-            {cars.map(car => (
-              <li key={car.id}>{car.make} - {car.model} - {car.year}</li>
-            ))}
-          </ul>
-        </section>
-        {/* Other static content */}
-      </div>
+    const [filter, setFilter] = useState('');
+
+    const handleFilterChange = (e) => {
+        setFilter(e.target.value.toLowerCase());
+    };
+
+    const filteredCars = cars.filter(car => 
+        car.make.toLowerCase().includes(filter) ||
+        car.model.toLowerCase().includes(filter) ||
+        car.year.toString().includes(filter)
     );
-  }
+
+    return (
+        <div>
+            <h1>Car Filter and Match</h1>
+            <section>
+                <input 
+                    type="text" 
+                    placeholder="Filter by make, model, or year" 
+                    value={filter} 
+                    onChange={handleFilterChange}
+                />
+            </section>
+            <section>
+                <h2>Matching Cars</h2>
+                <ul>
+                    {filteredCars.length > 0 ? (
+                        filteredCars.map(car => (
+                            <li key={car.id}>{car.make} - {car.model} - {car.year}</li>
+                        ))
+                    ) : (
+                        <li>No matching cars found.</li>
+                    )}
+                </ul>
+            </section>
+            {/* Other static content */}
+        </div>
+    )
+}
+
+export default MatchPage;
