e1084567f5216437d0fa15a7d1e32f0f9e74653e
yichl42 <86950411+yichl42@users.noreply.github.com>
2023-11-22 00:53:38 -0800

    adding some interactivity

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchSection() {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Searching for:', searchTerm);
+    };
+
     return (
       <section className="search-section">
         <h2>Find Your Dream Car</h2>
-        <form id="search-form">
+        <form id="search-form" onSubmit={handleSubmit}>
           <label htmlFor="search-input">Enter car details:</label>
-          <input type="text" id="search-input" aria-label="Car Search Input" placeholder="e.g., Brand, Model, Year" />
+          <input 
+              type="text" 
+              id="search-input" 
+              aria-label="Car Search Input" 
+              placeholder="Brand, Model, Year" 
+              value={searchTerm} 
+              onChange={(e) => setSearchTerm(e.target.value)}
+          />
           <button type="submit">Search</button>
         </form>
       </section>
     );
-  }  
+}
